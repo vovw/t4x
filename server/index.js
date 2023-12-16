3ad45fa3bd75ac705fc8c84b0cf6487c3c2ef778
atharva <voidz7447@gmail.com>
2023-12-17 05:04:42 +0530

    letsfuckinggooooooooooooooooooooooooo

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ import express from "express";
 import cors from "cors";
 import axios from "axios";
  
+import Replicate from "replicate";
+
+
+const replicate = new Replicate({
+  auth: "r8_bA8RxkDULTxUqPu5vVq7vKL9p8D1EsF1FpeGL",
+});
+
+
 const app = express()
 let corsOptions = {
 	origin: 'http://localhost:3000',
@@ -16,9 +24,34 @@ app.use(cors(corsOptions));
 
 
 
-app.post("/getmusic", (req,res)=>{
+
+app.post("/getmusic", async (req,res)=>{
 	let response=req.body;
-	console.log(response);
+	// console.log(response);
+	let user_prompt = response.data 
+	for(let i in response.sug){
+		user_prompt += ", " + response.sug[i];
+	}
+	const output = await replicate.run(
+	"meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
+	{
+	  input: {
+	 	    top_k: 250,
+	 	    top_p: 0,
+	 	    prompt: user_prompt,
+	 	    duration: 33,
+	 	    temperature: 1,
+	 	    continuation: false,
+	 	    model_version: "large",
+	 	    output_format: "mp3",
+	 	    continuation_start: 0,
+	 	    multi_band_diffusion: false,
+	 	    normalization_strategy: "peak",
+	 	    classifier_free_guidance: 3
+	 	  }
+	 	}
+	);
+	console.log(output);
 });
 
 

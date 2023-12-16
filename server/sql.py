3811dc1e993762db90b982c0e8cb24715ab68b32
atharva <voidz7447@gmail.com>
2023-12-16 14:34:17 +0530

    added sql.py

--- server/sql.py
+++ server/sql.py
@@ -12,9 +12,10 @@ cur.execute('''
     CREATE TABLE wav_files (
         id INTEGER PRIMARY KEY,
         wav_data BLOB NOT NULL,
-        file_name TEXT NOT NULL,
+        file_name TEXT NOT NULL
     )
 ''')
+
 
 
 # Define a function to insert a WAV file into the database
@@ -30,15 +31,20 @@ def insert_wav_file(file_path):
     # Insert the WAV file data into the database
     cur.execute('''
         INSERT INTO wav_files (wav_data, file_name )
-        VALUES (?, ?, ?, ?)
+        VALUES (?, ?)
     ''', (wav_data, file_name ))
 
     # Commit the transaction
     conn.commit()
 
-# Insert a WAV file into the database
-file_path = '/path/to/wav/file.wav'
-insert_wav_file(file_path)
+# # Insert a WAV file into the database
+# file_path = '/path/to/wav/file.wav'
+# insert_wav_file(file_path)
+folder_path = '/Users/asuna/generated/'
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    insert_wav_file(file_path)
+
 
 # Close the database connection
 conn.close()

b4c8204a6943f8ddb0720fa8fcc68d22695ef6bb
harshal-ishere <harshal011kamble.com>
2023-12-16 19:43:02 +0530

    did some frontedn shit

---
+++ client/src/components/PlayArea/bottomArea/bottomArea.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useRef, useEffect } from 'react'
+import styles from "./bottomArea.module.css";
+import { BsArrowLeftShort } from "react-icons/bs"
+import { BsArrowRightShort } from "react-icons/bs"
+import { FaPlay } from "react-icons/fa"
+import { FaPause } from "react-icons/fa"
+
+const AudioPlayer = () => {
+    // state
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+
+    // references
+    const audioPlayer = useRef();   // reference our audio component
+    const progressBar = useRef();   // reference our progress bar
+    const animationRef = useRef();  // reference the animation
+
+    useEffect(() => {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+        progressBar.current.max = seconds;
+    }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs / 60);
+        const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        return `${returnedMinutes}:${returnedSeconds}`;
+    }
+
+    const togglePlayPause = () => {
+        const prevValue = isPlaying;
+        setIsPlaying(!prevValue);
+        if (!prevValue) {
+            audioPlayer.current.play();
+            animationRef.current = requestAnimationFrame(whilePlaying)
+        } else {
+            audioPlayer.current.pause();
+            cancelAnimationFrame(animationRef.current);
+        }
+    }
+
+    const whilePlaying = () => {
+        progressBar.current.value = audioPlayer.current.currentTime;
+        changePlayerCurrentTime();
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+
+    const changeRange = () => {
+        audioPlayer.current.currentTime = progressBar.current.value;
+        changePlayerCurrentTime();
+    }
+
+    const changePlayerCurrentTime = () => {
+        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
+        setCurrentTime(progressBar.current.value);
+    }
+
+    const backThirty = () => {
+        progressBar.current.value = Number(progressBar.current.value - 5);
+        changeRange();
+    }
+
+    const forwardThirty = () => {
+        progressBar.current.value = Number(progressBar.current.value + 5);
+        changeRange();
+    }
+
+    return (
+        <div className={styles.audioPlayer + " flex flex-row justify-center items-center mt-20"}>
+            <section className="flex flex-row justify-center items-center pewww gap-8 px-4 rounded-2xl" >
+                <audio ref={audioPlayer} src="https://replicate.delivery/pbxt/FPpp2FjLbnpHGV0K0S7H9owPxVjoNbfW2xcIyAQb2LkF7aBJA/out.mp3" preload="metadata"></audio>
+                <div className='flex flex-row justify-center gap-3'>
+                    <button className={styles.forwardBackward} onClick={backThirty}><BsArrowLeftShort /> 5</button>
+                    <button onClick={togglePlayPause} className={styles.playPause}>
+                        {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
+                    </button>
+                    <button className={styles.forwardBackward} onClick={forwardThirty}>5 <BsArrowRightShort /></button>
+                </div>
+
+
+                {/* current time */}
+                <div className={styles.currentTime}>{calculateTime(currentTime)}</div>
+
+                {/* progress bar */}
+                <div className='flex flex-row items-center'>
+                    <input type="range" className={styles.progressBar} defaultValue="0" ref={progressBar} onChange={changeRange} />
+                </div>
+
+                {/* duration */}
+                <div className={styles.duration}>{(duration && !isNaN(duration)) && calculateTime(duration)}</div>
+            </section>
+        </div>
+    )
+}
+export { AudioPlayer }
--- client/src/components/PlayArea/middleArea/middleArea.jsx
+++ client/src/components/PlayArea/middleArea/middleArea.jsx
@@ -1,13 +1,22 @@
 import "./middleArea.css";
 
-export default function MiddleArea(){
-    return(
-        <section id="MiddleArea" className="flex flex-row justify-">
-            <div className="stableDiffusion">
-                <img src={require("../../../images/wp8130803.webp")} alt="" /> 
-            </div>
-            <div className="songDetails">
-                <p>Tokyo Rain v1</p>
+export default function MiddleArea() {
+    return (
+        <section id="MiddleArea" className="flex flex-row justify-center mt-10 rounded-3xl">
+            <div className="flex flex-row someMainArea rounded-3xl p-6">
+                <div className="stableDiffusion">
+                    <img src={require("../../../images/wp8130803.webp")} alt="" className="rounded-3xl" />
+                </div>
+                <div className="songDetails flex flex-col justify-start items-center gap-10 p-8 ">
+                    <p className="songName text-5xl">Tokyo Rain v1</p>
+                    <p className="songDes">welcome to the song details and descriptions about it</p>
+                    <div className="musicModBtn flex flex-row gap-6">
+                        <button>high speed</button>
+                        <button>low pitch</button>
+                        <button>lo-fi</button>
+                        <button>high bass</button>
+                    </div>
+                </div>
             </div>
         </section>
     )

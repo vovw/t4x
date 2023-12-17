6c91e6bcfe349e8349d91f0bd698e5e5f3fb04a6
Abhay Upadhyay <urabhay10@gmail.com>
2023-12-17 09:41:03 +0530

    dollar

--- client/src/components/PlayArea/playArea.jsx
+++ client/src/components/PlayArea/playArea.jsx
@@ -1,13 +1,29 @@
+import { Link } from "react-router-dom";
 import { AudioPlayer } from "./bottomArea/bottomArea";
 import MiddleArea from "./middleArea/middleArea";
-import "./playArea.css";
 
-export default function PLayArea(props){
-    const {audioLink, promptsug, promptdata}= props
-    return(
+import "./playArea.css";
+import { useState } from "react";
+import { AiOutlineDollar } from 'react-icons/ai';
+
+
+export default function PlayArea(props) {
+    const { audioLink, promptsug, promptdata } = props;
+    const [showButton, setShowButton] = useState(true);
+
+    const handleButtonClick = () => {
+        // Handle button click event here
+    };
+
+    return (
         <section id="PlayArea">
-            <MiddleArea promptdata={promptdata} promptsug={promptsug}/>
-            <AudioPlayer audioLink={audioLink}/>
-        </section>
-    )
+            <MiddleArea promptdata={promptdata} promptsug={promptsug} />
+            <AudioPlayer audioLink={audioLink} />
+
+            <button className="dollar-button">
+                <Link to="/pricing">
+                    <AiOutlineDollar size={48} /></Link>
+            </button>
+        </section >
+    );
 }
